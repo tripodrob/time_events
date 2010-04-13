967cedd4e2bceb8b291a33566249b94d7d55c335
Robbie Bingler <rwb3y@hero.village.Virginia.EDU>
2010-04-13 08:54:51 -0400

    install js and css

--- install.rb
+++ install.rb
@@ -5,6 +5,7 @@ require 'fileutils'
 model_dir = File.dirname(__FILE__) + "/../../../app/models"
 controller_dir = File.dirname(__FILE__) + "/../../../app/controllers"
 view_dir = File.dirname(__FILE__) + "/../../../app/views"
+public_dir = File.dirname(__FILE__) + "/../../../public"
 plugin_dir = File.dirname(__FILE__) + "/lib/app"
 
 FileUtils.install(plugin_dir + "/controllers/time_events_controller.rb", controller_dir)
@@ -20,3 +21,15 @@ FileUtils.install(plugin_dir + "/views/time_events/index.html.erb", view_dir + '
 FileUtils.install(plugin_dir + "/views/time_events/search_dates.html.erb", view_dir + '/time_events')
 FileUtils.install(plugin_dir + "/views/time_events/search_dates_action.html.erb", view_dir + '/time_events')
 FileUtils.install(plugin_dir + "/views/time_events/show.html.erb", view_dir + '/time_events')
+
+plugin_dir = File.dirname(__FILE__) + "/public"
+FileUtils.install(plugin_dir + "/javascripts/centerdecadedateslider.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/centerdecadedateviewer.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/ate-en-US.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/dateslider.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/dateviewer.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/decadedateslider.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/precisedateslider.js", public_dir + '/javascripts')
+FileUtils.install(plugin_dir + "/javascripts/precisedecadedateslider.js", public_dir + '/javascripts')
+
+FileUtils.install(plugin_dir + "/stylesheets/dateslider.css", public_dir + '/stylesheets')
