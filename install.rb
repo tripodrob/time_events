c6b1ccd16dfec231db024c0516a5201515d00f31
Robbie Bingler <rwb3y@hero.village.Virginia.EDU>
2010-04-13 07:24:37 -0400

    comment out

--- install.rb
+++ install.rb
@@ -1,20 +1,20 @@
-# Install hook code here
-
-model_dir = File.dirname(__FILE__) + "/../../../app/models"
-controller_dir = File.dirname(__FILE__) + "/../../../app/controllers"
-view_dir = File.dirname(__FILE__) + "/../../../app/views"
-plugin_dir = File.dirname(__FILE__) + "/app"
-
-FileUtils.install(plugin_dir + "/controllers/time_events_controller.rb", controller_dir)
-FileUtils.install(plugin_dir + "/models/time_event.rb", model_dir)
-FileUtils.install(plugin_dir + "/controllers/time_events_controller.rb", controller_dir)
-
-FileUtils.mkdir_p(views_dir + "/time_events")
-FileUtils.install(plugin_dir + "/views/time_events/_begin_date_start_form_column.rhtml", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/_event_duration.rhtml", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/_form.rhtml", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/edit.html.erb", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/index.html.erb", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/search_dates.html.erb", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/search_dates_action.html.erb", view_dir + '/time_events')
-FileUtils.install(plugin_dir + "/views/time_events/show.html.erb", view_dir + '/time_events')
+# # Install hook code here
+# 
+# model_dir = File.dirname(__FILE__) + "/../../../app/models"
+# controller_dir = File.dirname(__FILE__) + "/../../../app/controllers"
+# view_dir = File.dirname(__FILE__) + "/../../../app/views"
+# plugin_dir = File.dirname(__FILE__) + "/app"
+# 
+# FileUtils.install(plugin_dir + "/controllers/time_events_controller.rb", controller_dir)
+# FileUtils.install(plugin_dir + "/models/time_event.rb", model_dir)
+# FileUtils.install(plugin_dir + "/controllers/time_events_controller.rb", controller_dir)
+# 
+# FileUtils.mkdir_p(views_dir + "/time_events")
+# FileUtils.install(plugin_dir + "/views/time_events/_begin_date_start_form_column.rhtml", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/_event_duration.rhtml", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/_form.rhtml", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/edit.html.erb", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/index.html.erb", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/search_dates.html.erb", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/search_dates_action.html.erb", view_dir + '/time_events')
+# FileUtils.install(plugin_dir + "/views/time_events/show.html.erb", view_dir + '/time_events')
